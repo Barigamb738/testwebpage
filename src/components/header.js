ebd0b07e0184bc7f1b72106bf88aad6a4264e147
Barigamb738 <54997181+Barigamb738@users.noreply.github.com>
2020-09-15 18:47:18 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          My Website
         </Link>
       </h1>
     </div>
