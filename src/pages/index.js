4c8d31f689f089fb5ce687ca69b05bf9597c38a5
Barigamb738 <54997181+Barigamb738@users.noreply.github.com>
2020-09-15 17:43:40 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Szia Gabi</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <SEO title="Woeboldal" />
+    <h1>Ez az én weboldalam.</h1>
+    <p>Ez egy test.</p>
   </Layout>
 )
 
