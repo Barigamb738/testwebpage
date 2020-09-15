16ae600da858c1c56db489dcb141c53382503993
Barigamb738 <54997181+Barigamb738@users.noreply.github.com>
2020-09-15 19:01:09 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,7 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
+        color: `yellow`,
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
